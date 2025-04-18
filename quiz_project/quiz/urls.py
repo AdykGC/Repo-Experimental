a44410a4c936b50c86b51376c2e5cde9577def0e
Adilkhan Altynbek <adilkhanaltynbek@MBPA.local>
2025-04-18 23:33:25 +0500

    feat | .Env Css . gitignore

--- quiz_project/quiz/urls.py
+++ quiz_project/quiz/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.quiz_view, name='quiz'),
-    path('submit/', views.submit_quiz, name='submit_quiz'),
+    path('',              views.quiz_view,         name='quiz'),
+    path('submit/',       views.submit_quiz,       name='submit_quiz'),
 ]
--- quiz_project/quiz_project/settings.py
+++ quiz_project/quiz_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c2_8o7*p)xr!x%0fec+f_g^23mv3k*4*%3%ve&=ftg1!7hl&dr'
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = os.getenv('DEBUG') == 'True'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'quiz_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DB_Quiz',
-        'USER': 'postgres',
-        'PASSWORD': 'DDRM74df',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE':       os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME':         os.getenv('DB_NAME'),
+        'USER':         os.getenv('DB_USER'),
+        'PASSWORD':     os.getenv('DB_PASSWORD'),
+        'HOST':         os.getenv('DB_HOST', 'localhost'),
+        'PORT':         os.getenv('DB_PORT', '5432'),
     }
 }
 
@@ -109,13 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
@@ -128,3 +124,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Загружаем .env из BASE_DIR/.env
+load_dotenv(BASE_DIR / '.env')
